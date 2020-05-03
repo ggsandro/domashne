2360f84e8018af21863e7e623db238cfc91bf8db
Dmytro Titov <dmitry90titov@gmail.com>
2020-05-03 15:42:34 +0300

    setup commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+
+var path = require("path");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  entry: ["./src/styles/styles.scss"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    //filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          'css-loader',
+          //'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'style.css'
+      //chunkFilename: '[id].css',
+    }),
+  ],
+};

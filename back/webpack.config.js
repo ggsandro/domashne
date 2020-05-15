3d70222af5379e265f2ea653ee69613c59b78dc1
Dmytro Titov <dmitry90titov@gmail.com>
2020-05-15 22:24:29 +0300

    asp.net project home page + sass compilation

---
+++ back/webpack.config.js
@@ -0,0 +1,66 @@
+
+var path = require("path");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  entry: ["./styles/styles.scss"],
+  output: {
+    path: path.resolve(__dirname, "wwwroot/css"),
+    //filename: "bundle.js",
+    publicPath: "/wwwroot/css"
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './wwwroot/css',
+    hot: true,
+    historyApiFallback: true,
+    //contentBase: [path.join(__dirname, './index.html')],
+    watchContentBase: true,
+    noInfo: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: devMode,
+            },
+          },
+          'css-loader',
+          //'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]?[hash]'
+        }
+      }
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'style.css'
+      //chunkFilename: '[id].css',
+    }),
+    //new HtmlWebpackPlugin({
+      //title: 'Custom template',
+      // Load a custom template (lodash by default)
+      //template: './index.html'
+    //})
+  ],
+};
